6cbc226556b45458a137e2a6f070ad37752a1825
Benjamin DIDIER <contact@aziks.aleeas.com>
2022-03-30 19:19:22 +0200

    feat: add package formatting to markdown functions

--- src/analyse-package.ts
+++ src/analyse-package.ts
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import * as p from 'path';
+import * as md from './utils/markdown';
 
 type TPeople = string | { name: string; email?: string; url?: string };
 type TAuthor = TPeople;
@@ -33,4 +35,100 @@ const getDevDependencies = (path: string) => {
   return null;
 };
 
-export { getDevDependencies };
+const formatPeople = (people: TPeople) => {
+  // `<` needs to be escaped to avoid GitHub removing it
+  if (typeof people === 'string') return people.replace('<', '\\<');
+
+  let people_ = people.name;
+  // `<` needs to be escaped to avoid GitHub removing it
+  if (people.email) people_ += ` \\<${people.email}>`;
+  if (people.url) people_ += ` (${people.url})`;
+  return people_;
+};
+
+const formatContributors = (contributors: TContributors) => {
+  return contributors.map((contributor) => formatPeople(contributor));
+};
+
+const getRepository = (repository: TRepository) => {
+  return typeof repository === 'string' ? repository : repository.url;
+};
+
+/**
+ * Format a pair of key/value to the markdown string
+ * `**key:** value\n`
+ *
+ * @param key The text key
+ * @param value The text value
+ * @returns The string `**key:** value\n`
+ */
+const formatLine = (key: string, value: string) => {
+  return (
+    md.toEmphasis(key + ':', md.MarkdownEmphasis.BOLD) + ' ' + value + '\n'
+  );
+};
+
+const getLicenseFilename = (path: string) => {
+  const files = fs.readdirSync(path);
+  return files.find((file) => {
+    file = file.toLowerCase();
+    if (file.startsWith('license') || file.startsWith('licence')) return true;
+    return false;
+  });
+};
+
+const getPackageInfos = (path: string) => {
+  const packageJsonBuffer = fs.readFileSync(path);
+  const packageJson: PackageJson = JSON.parse(packageJsonBuffer.toString());
+
+  // Title
+  let markdownText = md.toHeader(
+    packageJson.name + '@' + packageJson.version,
+    md.MarkdownHeaders.H2
+  );
+
+  // Homepage
+  if (packageJson.homepage)
+    markdownText += formatLine('Homepage', packageJson.homepage);
+
+  // Repo
+  if (packageJson.repository) {
+    const repository = getRepository(packageJson.repository);
+    markdownText += formatLine('Repository', repository);
+  }
+
+  // Author
+  if (packageJson.author) {
+    const author = formatPeople(packageJson.author);
+    markdownText += formatLine('Author', author);
+  }
+
+  // Contributors
+  if (packageJson.contributors) {
+    const contributors = formatContributors(packageJson.contributors);
+    markdownText += md.toList(
+      contributors,
+      md.MarkdownLists.INDENTED,
+      'Contributors:',
+      true
+    );
+  }
+
+  // License type
+  if (packageJson.license)
+    markdownText += formatLine('License', packageJson.license);
+
+  // License text
+  const dirname = p.dirname(path);
+  const licenseFileName = getLicenseFilename(dirname);
+  if (!licenseFileName) return markdownText;
+
+  path = p.join(dirname, licenseFileName);
+  const buffer = fs.readFileSync(path);
+  const text = buffer.toString();
+  markdownText += md.toCode(text);
+
+  return markdownText;
+};
+
+export { getPackageJsonContent, getDevDependencies, getPackageInfos };
